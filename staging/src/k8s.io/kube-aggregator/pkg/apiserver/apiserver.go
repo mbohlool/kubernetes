ec2aa07c7b3e3059773e5899636e4cab66475d00
Mehdy Bohlool <mehdy@mehdy.cbf.corp.google.com>
2017-08-23 03:16:32 -0500

    fixup! Address more comments (to squash)

--- staging/src/k8s.io/kube-aggregator/pkg/apiserver/apiserver.go
+++ staging/src/k8s.io/kube-aggregator/pkg/apiserver/apiserver.go
@@ -17,7 +17,6 @@ limitations under the License.
 package apiserver
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -27,7 +26,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
 	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
 	"k8s.io/apiserver/pkg/registry/rest"
